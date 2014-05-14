49eca98857d6c4afc0aceea46caf3f9aa6b24a5a
Molinero <sv.melnik.87@gmail.com>
2014-05-14 14:20:29 +0200

    clean code in TestClass

--- msv-automaton/src/test/java/tests/TestClass.java
+++ msv-automaton/src/test/java/tests/TestClass.java
@@ -6,20 +6,6 @@ package tests;
 public class TestClass {
     public static void main(String[] args) {
 
-        int a = 1;
-        int b = 5;
-        System.out.println(a & b);
-        System.out.println(a | b);
-        System.out.println(a ^ b);
 
-    }
-
-    private static int f(){
-        try{
-            System.out.println(0);
-            return 100;
-        } finally {
-            System.out.println(1);
-        }
     }
 }
